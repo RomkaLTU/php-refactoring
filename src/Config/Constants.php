27305e2ab9bf5ad9be853fa3f95fc9fe4fbaf82b
RD <hello@codesomelabs.com>
2025-05-24 11:43:05 +0300

    Adds discount constants
    Defines constants for discount thresholds and rates.
    
    These constants centralize discount-related values, improving
    maintainability and making it easier to adjust discount policies in
    the future.

---
+++ src/Config/Constants.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Config;
+
+class Constants
+{
+    public const DISCOUNT_THRESHOLD = 100;
+    public const DISCOUNT_RATE = 0.1;
+    public const VIP_DISCOUNT_RATE = 0.1;
+}
